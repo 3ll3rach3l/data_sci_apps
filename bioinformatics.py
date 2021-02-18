d5050f96be985d185f0c6d78d7754fe41a6665be
Lauren Beard <lbeard06@yahoo.com>
2021-02-18 10:40:56 -0800

    complete

--- bioinformatics.py
+++ bioinformatics.py
@@ -57,3 +57,31 @@ X = DNA_nucleotide_count(sequence)
 
 X_label = list(X)
 X_values = list(X.values())
+
+X
+
+### 2. Print text
+st.subheader('2. Print text')
+st.write('There are ' + str(X['A']) + ' adenine (A)')
+st.write('There are ' + str(X['T']) + ' thymine (T)')
+st.write('There are ' + str(X['G']) + ' guanine (G)')
+st.write('There are ' + str(X['C']) + ' cytosine (C)')
+
+### 3. Display DataFrame
+st.subheader('3. Display DataFrame')
+df = pd.DataFrame.from_dict(X, orient='index')
+df = df.rename({0: 'count'}, axis='columns')
+df.reset_index(inplace=True)
+df = df.rename(columns = {'index': 'nucleotide'})
+st.write(df)
+
+### 4. Display Bar Chart using altair
+st.subheader('4. Display Bar chart')
+p = alt.Chart(df).mark_bar().encode(
+    x='nucleotide',
+    y='count'
+)
+p = p.properties(
+    width=alt.Step(80) #controls width of bar
+)
+st.write(p)
