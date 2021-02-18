fb406e251c8fd6083ce6c05bdfba37b29c492eae
Lauren Beard <lbeard06@yahoo.com>
2021-02-18 10:31:40 -0800

    creating different ways to display output

--- bioinformatics.py
+++ bioinformatics.py
@@ -29,7 +29,7 @@ sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
 sequence #this will show the list
 sequence = sequence[1:] #skips the sequence name (first line)
-sequence = ''.join(sequence) #concatenates list to string
+sequence = '\n'.join(sequence) #concatenates list to string
 
 st.write(""" 
 ***
@@ -43,3 +43,17 @@ sequence
 st.header('OUTPUT (DNA Nucleotide Count)')
 
 ### 1. Print Dictionary
+st.subheader('1. Print dictionary')
+def DNA_nucleotide_count(seq):
+    d = dict([
+        ('A',seq.count('A')),
+        ('T',seq.count('T')),
+        ('G',seq.count('G')),
+        ('C',seq.count('C'))
+    ])
+    return d
+
+X = DNA_nucleotide_count(sequence)
+
+X_label = list(X)
+X_values = list(X.values())
